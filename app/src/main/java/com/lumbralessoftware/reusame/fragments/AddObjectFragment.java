8c718536894339de6795e5e4e4109c06ea66770b
Cabezas <javiergonzalezcabezas@gmail.com>
2015-07-18 18:48:26 +0100

    added categories in spanish and minimus changes in addObjectFragment

--- app/src/main/java/com/lumbralessoftware/reusame/fragments/AddObjectFragment.java
+++ app/src/main/java/com/lumbralessoftware/reusame/fragments/AddObjectFragment.java
@@ -1,7 +1,11 @@
 package com.lumbralessoftware.reusame.fragments;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.DialogInterface;
@@ -11,6 +15,7 @@ import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Parcelable;
@@ -23,6 +28,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -35,6 +41,7 @@ import com.lumbralessoftware.reusame.controller.AddItemController;
 import com.lumbralessoftware.reusame.controller.ControllersFactory;
 import com.lumbralessoftware.reusame.interfaces.AddItemResponseHandler;
 import com.lumbralessoftware.reusame.interfaces.AddItemResponseListener;
+import com.lumbralessoftware.reusame.interfaces.DateListener;
 import com.lumbralessoftware.reusame.interfaces.ItemResponseListener;
 import com.lumbralessoftware.reusame.models.Item;
 import com.lumbralessoftware.reusame.models.Location;
@@ -43,6 +50,7 @@ import com.lumbralessoftware.reusame.utils.Utils;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -435,6 +443,36 @@ public class AddObjectFragment extends Fragment implements View.OnClickListener,
     }
     public void hideProgress() {
         mDialogLoading.dismiss();
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        public DatePickerFragment(DateListener mListener) {
+            this.mListener = mListener;
+        }
+
+        DateListener mListener;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            mListener.getDate(year, month, day);
+
+        }
     }
 
 }
---
+++ app/src/main/java/com/lumbralessoftware/reusame/interfaces/DateListener.java
@@ -0,0 +1,8 @@
+package com.lumbralessoftware.reusame.interfaces;
+
+/**
+ * Created by javiergonzalezcabezas on 18/7/15.
+ */
+public interface DateListener {
+    void getDate(int year, int month, int day);
+}
